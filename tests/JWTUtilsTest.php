c4562d007d347b37cc382fd38e17c970aeb258a7
Yan Nasonov <yan.nasonov@sap.com>
2023-06-18 17:33:43 +0300

    Improved test and testability for signature validate

--- tests/JWTUtilsTest.php
+++ tests/JWTUtilsTest.php
@@ -58,18 +58,24 @@ class JWTUtilsTest extends TestCase
      */
     public function testValidateSignature(string $apiKey, string $apiDomain, string $userKey, string $privateKey, string $uid)
     {
-        /* Requires at least two passes because getJWT behaves differently on the first pass, if the public key isn't cached. */
-        $passes = 2;
-
-        for ($i = 0; $i < $passes; $i++) {
-            $jwt = $this->getJWT($apiKey, $apiDomain, $userKey, $privateKey, $uid);
-            $this->assertNotFalse($jwt);
-
-            $claims = JWTUtils::validateSignature($jwt, $apiKey, $apiDomain);
-            $this->assertEquals($claims->apiKey, $apiKey);
-            $this->assertEquals($claims->sub, $uid);
-            $this->assertNotEmpty($claims->email);
+        $cacheFilePath = __DIR__ . '/../src/keys/' . $apiDomain . '_keys.txt';
+        if (file_exists($cacheFilePath)) {
+            unlink($cacheFilePath);
         }
+
+        $jwt = $this->getJWT($apiKey, $apiDomain, $userKey, $privateKey, $uid);
+        $this->assertNotFalse($jwt);
+
+        /* Requires at least two passes because getJWT behaves differently on the first pass, if the public key isn't cached. This is why ignoreCache is used */
+        $claims = JWTUtils::validateSignature($jwt, $apiKey, $apiDomain, true);
+        $this->assertEquals($claims->apiKey, $apiKey);
+        $this->assertEquals($claims->sub, $uid);
+        $this->assertNotEmpty($claims->email);
+
+        $claims = JWTUtils::validateSignature($jwt, $apiKey, $apiDomain);
+        $this->assertEquals($claims->apiKey, $apiKey);
+        $this->assertEquals($claims->sub, $uid);
+        $this->assertNotEmpty($claims->email);
     }
 
     /**
